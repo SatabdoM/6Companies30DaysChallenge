37ca2159d8b5039eaae9d2a50ee379d46dcea41b
SatabdoM <105154633+SatabdoM@users.noreply.github.com>
2023-01-08 01:16:31 +0530

    Add files via upload

---
+++ 15NumberOdfStringsContainingAll3Characters.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int numberOfSubstrings(string s) 
+{
+    int left = 0 , right = 0 , end = s.size() - 1;
+    unordered_map<char,int> map;
+    int count = 0;
+    int n=s.size();
+
+    while(right < n)
+    {
+        map[s[right]] += 1;
+        
+        while(map['a'] and map['b'] and map['c'])
+        {
+            cout<<count<<endl;
+            count += 1 + (end - right);
+            map[s[left]] -= 1; 
+            left++;
+        }
+        right++;
+    }
+    return count;
+}
